d0b52afff7433609f354a7adf0f07faaebe857e6
ajaycreation <ajaykumarsubramani26@gmail.com>
2024-12-16 16:35:47 +0530

    dashboard

--- resources/views/dashboards/studentdashboard.blade.php
+++ resources/views/dashboards/studentdashboard.blade.php
@@ -10,42 +10,42 @@
                 <!-- Profile Icon -->
                 <div class="col-md-2 col-sm-4 col-6 mb-4">
                     <a href="#" class="btn btn-lg  w-100" style="padding: 20px;background-color: #0cddf8; color: white;">
-                        <i class="fas fa-user fa-2x mb-2"></i><br>
+                        <i data-feather="user" class="fa-2x mb-2"></i><br>
                         PROFILE
                     </a>
                 </div>
                 <!-- Class Videos -->
                 <div class="col-md-2 col-sm-4 col-6 mb-4">
                     <a href="#" class="btn btn-lg  w-100" style="padding: 20px;background-color: #fa0f0f; color: white;">
-                        <i class="fas fa-video fa-2x mb-2"></i><br>
+                        <i data-feather="video" class="fa-2x mb-2"></i><br>
                         CLASS VIDEOS
                     </a>
                 </div>
                 <!-- Exam Portions -->
                 <div class="col-md-2 col-sm-4 col-6 mb-4">
                     <a href="#" class="btn btn-lg  w-100" style="padding: 20px;background-color: #a502c5; color: white;">
-                        <i class="fas fa-file-alt fa-2x mb-2"></i><br>
+                        <i data-feather="file-text" class="fa-2x mb-2"></i><br>
                         EXAM PORTIONS
                     </a>
                 </div>
                 <!-- Question Paper -->
                 <div class="col-md-2 col-sm-4 col-6 mb-4">
                     <a href="#" class="btn btn-lg  w-100" style="padding: 20px;background-color: #fa6806; color: white;">
-                        <i class="fas fa-file fa-2x mb-2"></i><br>
+                        <i data-feather="file" class="fa-2x mb-2"></i><br>
                         QUESTION PAPER
                     </a>
                 </div>
                 <!-- Answer Key -->
                 <div class="col-md-2 col-sm-4 col-6 mb-4">
                     <a href="#" class="btn btn-lg  w-100" style="padding: 20px;background-color: #fa5103; color: white;">
-                        <i class="fas fa-key fa-2x mb-2"></i><br>
+                        <i data-feather="key" class="fa-2x mb-2"></i><br>
                         ANSWER KEY
                     </a>
                 </div>
                 <!-- Downloads -->
                 <div class="col-md-2 col-sm-4 col-6 mb-4">
                     <a href="#" class="btn btn-lg  w-100" style="padding: 20px; background-color: #0a1af1; color: white;">
-                        <i class="fas fa-download fa-2x mb-2"></i><br>
+                        <i data-feather="download" class="fa-2x mb-2"></i><br>
                         DOWNLOADS
                     </a>
                 </div>
--- resources/views/dashboards/teacherdashboard.blade.php
+++ resources/views/dashboards/teacherdashboard.blade.php
@@ -7,7 +7,24 @@
         <div class="col-12">
             <h3><strong>Home</strong></h3>
             <div class="row text-center">
+                <div class="col-md-2 col-sm-4 col-6 mb-4">
+                    <a href="#" class="btn btn-lg  w-100" style="padding: 20px;background-color: #0cddf8; color: white;">
+                        <i data-feather="user" class="fa-2x mb-2"></i><br>
+                        PROFILE
+                    </a>
             </div>
+            <div class="col-md-2 col-sm-4 col-6 mb-4">
+                <a href="#" class="btn btn-lg  w-100" style="padding: 20px;background-color: #0cddf8; color: white;">
+                    <i data-feather="clock" class="fa-2x mb-2"></i><br>
+                    TIME TABLE
+                </a>
+        </div>
+        <div class="col-md-2 col-sm-4 col-6 mb-4">
+            <a href="#" class="btn btn-lg  w-100" style="padding: 20px;background-color: #0cddf8; color: white;">
+                <i data-feather="check-square" class="fa-2x mb-2"></i><br>
+                ATTENDENCE MARKUP
+            </a>
+    </div>
         </div>
     </div>
 </div>
