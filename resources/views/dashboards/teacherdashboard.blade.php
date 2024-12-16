3114b86b234f448d30dd5812d17fe3b074eaff7f
ajaycreation <ajaykumarsubramani26@gmail.com>
2024-12-16 16:57:46 +0530

    teacherdashboard

--- resources/views/dashboards/teacherdashboard.blade.php
+++ resources/views/dashboards/teacherdashboard.blade.php
@@ -5,7 +5,7 @@
 <section class="section">
     <div class="row">
         <div class="col-12">
-            <marquee behavior="scroll" direction="left" style="background-color: #e61515cb; padding: 10px; color: white; font-weight: bold;">Today is {{ date('l, F jS') }} and your upcoming class is :</marquee>
+            <marquee behavior="scroll" direction="left" style="background-color: #e61515cb; padding: 10px; color: white; font-weight: bold;">****** Today is {{ date('l, F jS') }} and your upcoming class is :CHEMISTRY *****</marquee>
             <h3><strong>Home</strong></h3>
             <div class="row text-center">
                 <div class="col-md-2 col-sm-4 col-6 mb-4">
@@ -25,6 +25,12 @@
                 <i data-feather="check-square" class="fa-2x mb-2"></i><br>
                 ATTENDENCE MARKUP
             </a>
+    </div>
+    <div class="col-md-2 col-sm-4 col-6 mb-4">
+        <a href="#" class="btn btn-lg  w-100" style="padding: 20px;background-color: #fa5103; color: white;">
+            <i data-feather="book" class="fa-2x mb-2"></i><br>
+            NOTES
+        </a>
     </div>
         </div>
     </div>
