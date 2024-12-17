28dae836f3aacf4f08496c04faf339c8128cc4ac
ajaycreation <ajaykumarsubramani26@gmail.com>
2024-12-17 12:06:12 +0530

    csv parser

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ImportController;
 
 /*
 |-------------------------------------------------------------------------- 
@@ -36,7 +37,8 @@ return view('dashboards.studentdashboard');
 Route::get('teacherdashboard', function () {
 return view('dashboards.teacherdashboard');
 })->name('teacherdashboard');
+
+Route::get('student/import', [ImportController::class, 'index'])->name('import');
+Route::post('student/import/upload', [ImportController::class, 'upload'])->name('import.upload');
 });
-
-
 
