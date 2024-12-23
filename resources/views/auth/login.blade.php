391e794aa5e1902218872ad5c83f903b940798b6
ajaycreation <ajaykumarsubramani26@gmail.com>
2024-12-23 13:19:34 +0530

    title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>ERP System Student Login</title>
+  <title>Login</title>
   <!-- General CSS Files -->
   <link rel="stylesheet" href="{{asset('css/app.min.css')}}">
   <link rel="stylesheet" href="{{asset('css/login.css')}}">
