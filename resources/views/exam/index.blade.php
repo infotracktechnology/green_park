aa64d8a0bfbd21c82d7b209c892e58e7d6c053bf
infotracktechnology <infotracktechnologysolutions@gmail.com>
2025-01-02 11:29:36 +0530

    update

--- resources/views/exam/index.blade.php
+++ resources/views/exam/index.blade.php
@@ -88,7 +88,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="formModal">Test Shechedule</h5>
+          <h5 class="modal-title">Test Schedule</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
