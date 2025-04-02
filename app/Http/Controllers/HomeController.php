abfbc07b00a455f00b851ea3a4e936bec78b337e
infotracktechnology <infotracktechnologysolutions@gmail.com>
2025-04-02 17:00:09 +0530

    done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers;
 use App\Models\Student;
-
 use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $activeUsersCount = Student::where('active', 1)->count();
 
-        
-
+        if($request->has('academic_year')) {
+            session()->put('academic_year', $request->academic_year);
+        }       
+       
         return view('home', compact('activeUsersCount'));
     }
 
