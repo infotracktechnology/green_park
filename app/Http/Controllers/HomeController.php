f1666bd9ef06d44cbbb6ab27604a94ccbcd2f863
infotracktechnology <infotracktechnologysolutions@gmail.com>
2025-04-02 17:15:43 +0530

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\AcademicYear;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 //use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
@@ -14,7 +15,7 @@ class HomeController extends Controller
         $activeUsersCount = Student::where('active', 1)->count();
 
         if($request->has('academic_year')) {
-            AcademicYear::update(['active' => 0]);
+            DB::table('academic_year')->update(['active' => 0]);
             AcademicYear::where('academic_year', $request->academic_year)->update(['active' => 1]);
         }       
        
