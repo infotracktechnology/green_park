ce0c4ce2bf9fefbe64dd8ef33c30a781f5091384
infotracktechnology <infotracktechnologysolutions@gmail.com>
2025-04-12 17:15:31 +0530

    code

--- app/Http/Controllers/TimetableController.php
+++ app/Http/Controllers/TimetableController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Timetable;
+use App\Models\TimetableAssign;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
@@ -14,11 +15,39 @@ class TimetableController extends Controller
     public function index(Request $request)
     {
         $timetables = Timetable::all();
-        $sections = DB::table('student')->distinct()->select('section')->get();
-        if($request->has('section')) {
-            $timetables = Timetable::find($request->id)->update(['section' => implode(',', $request->section)]);
-            return to_route('timetable.index')->with('success', 'Timetable Assigned Section successfully');
+        $sections = [];
+
+        if($request->has('submit')) {
+            $timetable = Timetable::find($request->id);
+            $periods=[];
+            foreach($timetable->structure as $key => $section) {
+                $periods[$key] = array_map(function($item) {
+                    return [
+                        'period' => $item['period'],
+                        'subject' => '',
+                    ];
+                }, $section);
         }
+        
+        $assign = collect($request->section)->map(function ($section, $key) use ($periods, $request) { 
+            return [
+                'branch_id' => $request->branch_id,
+                'coaching_type' => $request->coaching_type,
+                'section' => $section,
+                'periods' => $periods,
+            ];
+        })->toArray();
+
+        $timetable->assign()->delete();
+        $timetable->assign()->createMany($assign);
+        return to_route('timetable.index')->with('success', 'Timetable Assigned Section successfully');
+        }
+
+        if($request->has('coaching_type') && $request->has('branch_id')){
+            $sections = DB::table('student')->where('campus', $request->branch_id)->where('coaching_type', $request->coaching_type)->distinct()->select('section')->get();
+            return response()->json($sections);
+        }
+            
         return view('timetable.index', compact('timetables', 'sections'));
     }
 
