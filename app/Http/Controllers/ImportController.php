26e3d86f2c22ef1e2f3072b56da05215028e2a96
infotracktechnology <infotracktechnologysolutions@gmail.com>
2025-05-07 16:37:51 +0530

    loop

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -41,9 +41,9 @@ class ImportController extends Controller
             if (!empty($data)) {
                 foreach ($data as $row) {
                     
-                    $row = array_map(function($key,$value) {
-                        return is_string($value) && $key != 'password_1'   ? mb_convert_case($value, MB_CASE_TITLE, 'UTF-8') : $value;
-                    }, $row);
+                    foreach($row as $key => $value){
+                        $row[$key] = is_string($value) && $key != 'password_1'   ? mb_convert_case($value, MB_CASE_TITLE, 'UTF-8') : $value;
+                    }
                     
 
                     $student_id = $row['student_id'] ?? 0;
