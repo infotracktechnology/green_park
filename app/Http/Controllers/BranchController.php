a3633f8b190fe29f0ce0719a3dd7eb4ddc9fbd5c
poojaravi <poojaravi0533@gmail.com>
2025-05-07 11:51:49 +0530

    ok

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -27,11 +27,11 @@ class BranchController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'mob_no' => ['unique:branch,mob_no', 'numeric', 'min:10'],
-            'email' => ['unique:branch,email', 'email'],
-            'file' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
+        // $request->validate([
+        //     'mob_no' => ['unique:branch,mob_no', 'numeric', 'min:10'],
+        //     'email' => ['unique:branch,email', 'email'],
+        //     'file' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        // ]);
     
         $data = $request->except('file'); // exclude file for now
     
@@ -57,11 +57,11 @@ class BranchController extends Controller
 
     public function update(Request $request, Branch $branch)
 {
-    $request->validate([
-        'mob_no' => ['unique:branch,mob_no,' . $branch->id, 'numeric', 'min:10'],
-        'email' => ['unique:branch,email,' . $branch->id, 'email'],
-        'file' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-    ]);
+    // $request->validate([
+    //     'mob_no' => ['unique:branch,mob_no,' . $branch->id, 'numeric', 'min:10'],
+    //     'email' => ['unique:branch,email,' . $branch->id, 'email'],
+    //     'file' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    // ]);
 
     
     $data = $request->except('file');
