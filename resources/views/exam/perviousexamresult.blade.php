87fb4b19c94f95116be9aac67a16148b781e39ba
infotracktechnology <infotracktechnologysolutions@gmail.com>
2025-12-31 18:26:24 +0530

    Previous Exam Result

--- resources/views/exam/perviousexamresult.blade.php
+++ resources/views/exam/perviousexamresult.blade.php
@@ -11,10 +11,12 @@
   <section class="section">
     <div class="section-body">
       <div class="card card-primary">
+
         <form method="get" action="{{ route('exam.perviousexamresult') }}">
           <div class="card-header">
             <h4>Previous Exam Result</h4>
           </div>
+
           <div class="card-body">
             <div class="row">
               <div class="form-group col-lg-4">
@@ -22,56 +24,84 @@
                 <select name="testcategory" id="testcategory" class="select2" required>
                   <option value="">Select Category</option>
                   @foreach ($category as $row)
-                  <option value="{{ $row }}" @selected($row==request('testcategory'))>{{ $row }}</option>
+                  <option value="{{ $row }}" @selected($row==request('testcategory'))>
+                    {{ $row }}
+                  </option>
                   @endforeach
                 </select>
               </div>
+
               <div class="form-group col-lg-4">
                 <label>Exam Name</label>
                 <select name="examname" id="examname" class="select2" required>
                   <option value="">Select Test</option>
                   @foreach ($exam as $row)
-                  <option value="{{ $row->subject }}" @selected($row->subject==request('examname'))>{{ $row->subject }}</option>
+                  <option value="{{ $row->subject }}" @selected($row->subject == request('examname'))>
+                    {{ $row->subject }}
+                  </option>
                   @endforeach
                 </select>
               </div>
+
               <div class="form-group col-lg-2">
                 <label>&nbsp;</label>
-                <button type="submit" class="btn btn-primary btn-block">Show Marks</button>
+                <button type="submit" class="btn btn-primary btn-block">
+                  Show Marks
+                </button>
               </div>
             </div>
 
-            @if($exams->count())
-            <div class="table-responsive">
-              <table class="table">
-                <?php
-                  $first = $exams->first();
-                ?>
+            @if($exams->isNotEmpty())
+            <?php
+            $first = $exams->first();
+            $subjects = [
+            'PHYSICS' => ['tot' => 'ptot', 'r' => 'pr', 'w' => 'pw', 'l' => 'pl'],
+            'CHEMISTRY' => ['tot' => 'ctot', 'r' => 'cr', 'w' => 'cw', 'l' => 'cl'],
+            'BOTANY' => ['tot' => 'btot', 'r' => 'br', 'w' => 'bw', 'l' => 'bl'],
+            'ZOOLOGY' => ['tot' => 'ztot', 'r' => 'zr', 'w' => 'zw', 'l' => 'zl'],
+            ];
+            ?>
+            <div class="table-responsive mt-3">
+              <table class="table table-bordered" id="examTable">
                 <thead>
                   <tr>
                     <th>#</th>
                     <th>STUDENT ID</th>
                     <th>STUDENT NAME</th>
                     <th>SECTION</th>
-                    <th>PHYSICS</th>
-                    <th>CHEMISTRY</th>
-                    <th>BOTANY</th>
-                    <th>ZOOLOGY</th>
+                    @if($first->tot)
+                    <th>{{ \Str::between($first->category, '(', ')') }}</th>
+                    @else
+                    @foreach($subjects as $name => $cols)
+                    <th>{{ $name }}</th>
+                    @endforeach
+                    @endif
                     <th>TOTAL</th>
                   </tr>
                 </thead>
+
                 <tbody>
-                  @foreach($exams as $key=>$row)
+                  @foreach($exams as $key => $row)
                   <tr>
-                    <td>{{ $key+1 }}</td>
+                    <td>{{ $key + 1 }}</td>
                     <td>{{ $row->stuid }}</td>
                     <td>{{ $row->sname }}</td>
                     <td>{{ $row->sec }}</td>
-                    <td>{{ $row->ptot }} / {{ ($row->pr+$row->pw+$row->pl)* 4 }}</td>
-                    <td>{{ $row->ctot }} / {{ ($row->cr+$row->cw+$row->cl)* 4 }}</td>
-                    <td>{{ $row->btot }} / {{ ($row->br+$row->bw+$row->bl)* 4 }}</td>
-                    <td>{{ $row->ztot }} / {{ ($row->zr+$row->zw+$row->zl)* 4 }}</td>
-                    <td>{{ $row->nettot}} / {{ $row->totmark }}</td>
+                    
+                    @if($row->tot)
+                    <td>
+                      {{ $row->tot}} / {{ ($row->r + $row->w + $row->l) * 4 }}
+                    </td>
+                    @else
+                    @foreach($subjects as $cols)
+                    <td>
+                      @if($row->{$cols['tot']})
+                      {{ $row->{$cols['tot']} }} / {{ ($row->{$cols['r']} + $row->{$cols['w']} + $row->{$cols['l']}) * 4 }}
+                      @endif
+                    </td>
+                    @endforeach
+                    @endif
+                    <td>{{ $row->tot ? $row->tot : $row->nettot }} / {{ $row->totmark }}</td>
                   </tr>
                   @endforeach
                 </tbody>
@@ -93,24 +123,18 @@
 <script src="{{ asset('bundles/datatables/export-tables/jszip.min.js') }}"></script>
 
 <script>
-  $('#testcategory').change(() => {
-    const category = $('#testcategory').val();
+  $('#testcategory').on('change', function () {
+    const category = $(this).val();
     window.location = `{{ route('exam.perviousexamresult') }}?testcategory=${category}`;
   });
   
-  $(function() {
-   try {
-    $('.table').DataTable({
+  $(document).ready(function () {
+      $('#examTable').DataTable({
         dom: 'Bfrtip',
         buttons: ['csv', 'excel'],
         searching: false,
-        retrieve: true,
         destroy: true,
-    });
-  } catch (error) {
-    console.error('DataTables initialization failed:', error);
-  }
+      });
   });
-  
 </script>
 @endsection
