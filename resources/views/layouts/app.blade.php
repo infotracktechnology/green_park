17774ea4e7d39e5fd563dd9da1072dcffe82869f
infotracktechnology <infotracktechnologysolutions@gmail.com>
2026-02-27 10:14:40 +0530

    Mock Test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -140,7 +140,6 @@
                 <li><a href="{{ route('worksheet.index') }}" class="nav-link">Worksheet</a></li>
                 <li><a href="{{ route('achievement.index') }}" class="nav-link">NEET Achievement</a></li>
                 <li><a href="{{ route('parent_concern') }}" class="nav-link">Parent Concern</a></li>
-                <li><a href="{{ route('mocktest.index') }}" class="nav-link">Mock Test <small> (shechedule)</small></a></li>
               </ul>
             </li>
             <li class="dropdown">
@@ -151,6 +150,7 @@
                 <li><a href="{{ route('exam.offlineexam') }}" class="nav-link">Add Offline Exam</a></li>
                 <li><a href="{{ route('exam.testcategory') }}" class="nav-link">Test Category <small>(add / delete)</small></a></li>
 
+                <li><a href="{{ route('mocktest.index') }}" class="nav-link">Mock Test <small> (shechedule)</small></a></li>
                 <li><a href="{{ route('exam.enable') }}" class="nav-link">Enable Test</a></li>
                 <li><a href="{{ route('exam.onlineresponse') }}" class="nav-link">Online Response(csv file)</a></li>
                 <li><a href="{{ route('exam.offline.upload')}}" class="nav-link">Offline/OMR Upload</a></li>
