6d83ed597bb8981a1714ed8e8b58449fcb199ee3
infotracktechnology <infotracktechnologysolutions@gmail.com>
2026-05-21 14:57:23 +0530

    student_exam

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -224,10 +224,6 @@ class ExamController extends Controller
         $maxQuestions = $answers->first()->q_no ?? 0;
         $answers = $answers->keyBy('q_no');
         $second = now()->diffInSeconds(Carbon::parse($exam->end_at), false);
-
-        // if($answers->count() >= $exam->total_questions) {
-        //     return redirect()->route('studentdashboard')->with('error', 'You have already attempted this Exam!');
-        // }
         
         if ($second < 0) {
             return redirect()->route('studentdashboard')->with('error', 'Exam Time is up!');
