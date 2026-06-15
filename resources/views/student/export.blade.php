c546042914d698510d4cfa7a1c16a4fa3655fd94
Aathiskumar2004 <aathiskumaraathis@gmail.com>
2026-06-15 11:57:52 +0530

    Export_student_filter

--- resources/views/student/export.blade.php
+++ resources/views/student/export.blade.php
@@ -20,30 +20,86 @@ $details = [
           <div class="row">
               <div class="col-12">
                   <div class="card card-primary" x-data="app">
+                     
+                     @if(session('error'))
+                         <div class="alert alert-danger m-3">{{ session('error') }}</div>
+                     @endif
+
                      <form method="get" id="myForm" target="_blank"  action="{{ route('export.student') }}" enctype="multipart/form-data">
                       <div class="card-body">
-                      <div class="row">
-
-                        <div class="col-md-12 col-sm-12 mb-3">
-                            <h6 class="col-deep-purple">Export Students</h6>
-                        </div>
-     
-                        @foreach ($details as $key => $value)
+                      
                         <div class="row">
-                        <div class="form-group col-lg-12">
-                            <h4>{{ $key }}</h4>
+                            <div class="col-md-12 col-sm-12 mb-3">
+                                <h6 class="col-deep-purple">Filter Students</h6>
+                            </div>
+                            
+                  <div class="form-group col-lg-3">
+                    <label for="branch">Select Academic Year:</label>
+                    <select name="academic_year" id="academic_year" class="form-control form-control-sm">
+                      @foreach ($academic_years as $row)
+                      <option value="{{ $row }}" @selected(request('academic_year') == $row)>{{ $row }}</option>
+                      @endforeach
+                    </select>
+                  </div>
+                            
+                            <div class="form-group col-lg-3">
+                                <label>Course</label>
+                                <select name="course" class="form-control">
+                                    <option value="">All</option>
+                                    @foreach($courses as $course)
+                                        <option value="{{ $course }}">{{ $course }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group col-lg-3">
+                                <label>Branch</label>
+                                <select name="branch" class="form-control">
+                                    <option value="">All</option>
+                                    @foreach($branches as $branch)
+                                        <option value="{{ $branch->id }}">{{ $branch->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group col-lg-3">
+                                <label>Coaching Type</label>
+                                <select name="coaching_type" class="form-control">
+                                    <option value="">All</option>
+                                    @foreach($coaching_types as $type)
+                                        <option value="{{ $type }}">{{ $type }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div>
-                        @foreach(explode(',', $value) as $field)
-                        <div class="form-group col-lg-3">
-                            <input type="checkbox" name="fields[]" value="{{ $field }}"> {{ $field }}
+
+                        <hr>
+
+                        <div class="row mt-3">
+                            <div class="col-md-12 col-sm-12 mb-3">
+                                <h6 class="col-deep-purple">Select Fields to Export</h6>
+                            </div>
+         
+                            @foreach ($details as $key => $value)
+                            <div class="row col-12">
+                                <div class="form-group col-lg-12">
+                                    <h4 class="mt-2">{{ $key }}</h4>
+                                </div>
+                                @foreach(explode(',', $value) as $field)
+                                <div class="form-group col-lg-3">
+                                    <label>
+                                        <input type="checkbox" name="fields[]" value="{{ $field }}"> {{ $field }}
+                                    </label>
+                                </div>
+                                @endforeach
+                            </div>
+                            @endforeach
+                            
+                            <div class="form-group col-lg-12 mt-3">
+                                <button type="submit" class="btn btn-primary">Export Data</button>
+                            </div>
                         </div>
-                        @endforeach
-                        </div>
-                        @endforeach
-                        <div class="form-group col-lg-3">
-                            <button type="submit" class="btn btn-primary mt-4">Export</button>
-                        </div>
-                      </div>
+
                       </div>
                      </form>
                   </div>
