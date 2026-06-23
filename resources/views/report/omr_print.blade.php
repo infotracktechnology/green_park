e501b300fc4887f389f6eb4fa0a0c29bec21c511
infotracktechnology <infotracktechnologysolutions@gmail.com>
2026-06-23 16:50:19 +0530

    answer

--- resources/views/report/omr_print.blade.php
+++ resources/views/report/omr_print.blade.php
@@ -23,7 +23,7 @@
     <div class="title">Q Type: {{ $answer->pluck('subject')->unique()->implode(',') }}</div>
      <table style="width: 100%;">
     <tr>
-    <?php $cols = $answer->chunk(45); ?>
+    <?php $cols = $answer->chunk($answer->count()/4); ?>
       @foreach($cols as $col)
       <td style="padding: 0px 10px;vertical-align: top;">
       <table class="table"  style="width: {{ 25 * count($cols) }}%;">
