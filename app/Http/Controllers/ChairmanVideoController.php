25b28ba237e484b6ad75f68f15e034935495ac8a
infotracktechnology <infotracktechnologysolutions@gmail.com>
2026-08-21 11:10:55 +0530

    Log

--- app/Http/Controllers/ChairmanVideoController.php
+++ app/Http/Controllers/ChairmanVideoController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Models\Chairmanvideo;
 use App\Providers\FcmServiceProvider;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ChairmanVideoController extends Controller
 {
